7f0f2b4746462608eb7728b6a4c70ca574b79976
greath91 <pablo_gf91@hotmail.com>
2024-02-27 20:33:11 +0100

    Refactorización completa

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,34 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner teclado=new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
 
-        int dato=teclado.nextInt();
-        int[] vector =new int[dato];
-        System.out.println("\nVector inicial hasta :"+dato);
+        int dato = teclado.nextInt();
+        int[] vector = new int[dato];
+        System.out.println("\nVector inicial hasta:" + dato);
 
         mostrarVectorInicial(vector);
 
         vector = Criba.generarPrimos(dato);
-        System.out.println("\nVector de primos hasta:"+dato);
+        System.out.println("\nVector de primos hasta:" + dato);
 
         mostrarVectorPrimos(vector);
     }
 
     private static void mostrarVectorPrimos(int[] vector) {
         for (int i = 0; i < vector.length; i++) {
-            if (i%10==0) System.out.println();
-            System.out.print(vector[i]+"\t");
+            if (i % 10 == 0) System.out.println();
+            System.out.print(vector[i] + "\t");
         }
     }
 
     private static void mostrarVectorInicial(int[] vector) {
         for (int i = 0; i < vector.length; i++) {
-            if (i%10==0) System.out.println();
-            System.out.print(i+1+"\t");
+            if (i % 10 == 0) System.out.println();
+            System.out.print(i + 1 + "\t");
         }
     }
 }
